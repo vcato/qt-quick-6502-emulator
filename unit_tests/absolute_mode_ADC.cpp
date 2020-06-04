e1fd0e5ecfd6659da0b23fc06a631b8e1801fc99
David Harrison <16119203+mr-mocap@users.noreply.github.com>
2020-06-03 23:12:12 -0400

    Add two more ADC mode tests.

--- unit_tests/absolute_mode_ADC.cpp
+++ unit_tests/absolute_mode_ADC.cpp
@@ -3,26 +3,30 @@
 
 
 
-struct ADC_Immediate_Expectations
+struct ADC_Absolute_Expectations
 {
-    constexpr ADC_Immediate_Expectations &accumulator(const uint8_t v) { a = v; return *this; }
+    constexpr ADC_Absolute_Expectations &accumulator(const uint8_t v) { a = v; return *this; }
 
-    uint8_t   a;
+    uint8_t a;
     NZCVFlags flags;
+
+    uint8_t   addend;
 };
 
-using ADCImmediate     = ADC<Immediate, ADC_Immediate_Expectations, 2>;
-using ADCImmediateMode = ParameterizedInstructionExecutorTestFixture<ADCImmediate>;
+using ADCAbsolute     = ADC<Absolute, ADC_Absolute_Expectations, 4>;
+using ADCAbsoluteMode = ParameterizedInstructionExecutorTestFixture<ADCAbsolute>;
 
 
 template<>
 void LoadInstructionIntoMemoryAndSetRegistersToInitialState(      InstructionExecutorTestFixture &fixture,
-                                                            const ADCImmediate                   &instruction_param)
+                                                            const ADCAbsolute                    &instruction_param)
 {
     fixture.loadOpcodeIntoMemory(instruction_param.operation,
-                                 AddressMode_e::Immediate,
+                                 AddressMode_e::Absolute,
                                  instruction_param.address.instruction_address);
-    fixture.fakeMemory[instruction_param.address.instruction_address + 1] = instruction_param.address.immediate_value;
+    fixture.fakeMemory[instruction_param.address.instruction_address + 1] = fixture.loByteOf(instruction_param.address.absolute_address);
+    fixture.fakeMemory[instruction_param.address.instruction_address + 2] = fixture.hiByteOf(instruction_param.address.absolute_address);
+    fixture.fakeMemory[instruction_param.address.absolute_address       ] = instruction_param.requirements.initial.addend;
 
     // Load appropriate registers
     fixture.r.a = instruction_param.requirements.initial.a;
@@ -34,7 +38,7 @@ void LoadInstructionIntoMemoryAndSetRegistersToInitialState(      InstructionExe
 
 template<>
 void RegistersAreInExpectedState(const Registers &registers,
-                                 const ADC_Immediate_Expectations &expectations)
+                                 const ADC_Absolute_Expectations &expectations)
 {
     EXPECT_THAT(registers.a, Eq(expectations.a));
     EXPECT_THAT(registers.GetFlag(FLAGS6502::N), Eq(expectations.flags.n_value.expected_value));
@@ -45,87 +49,126 @@ void RegistersAreInExpectedState(const Registers &registers,
 
 template<>
 void MemoryContainsInstruction(const InstructionExecutorTestFixture &fixture,
-                               const Instruction<AbstractInstruction_e::ADC, Immediate> &instruction)
+                               const Instruction<AbstractInstruction_e::ADC, Absolute> &instruction)
 {
-    EXPECT_THAT(fixture.fakeMemory.at( fixture.executor.registers().program_counter ), Eq( OpcodeFor(AbstractInstruction_e::ADC, AddressMode_e::Immediate) ));
-    EXPECT_THAT(fixture.fakeMemory.at( fixture.executor.registers().program_counter + 1), Eq(instruction.address.immediate_value));
+    EXPECT_THAT(fixture.fakeMemory.at( fixture.executor.registers().program_counter ), Eq( OpcodeFor(AbstractInstruction_e::ADC, AddressMode_e::Absolute) ));
+    EXPECT_THAT(fixture.fakeMemory.at( fixture.executor.registers().program_counter + 1), Eq( fixture.loByteOf(instruction.address.absolute_address) ));
+    EXPECT_THAT(fixture.fakeMemory.at( fixture.executor.registers().program_counter + 2), Eq( fixture.hiByteOf(instruction.address.absolute_address) ));
 }
 
 template<>
-void MemoryContainsExpectedComputation(const InstructionExecutorTestFixture &/* fixture */,
-                                       const ADCImmediate                   &/* instruction */)
+void MemoryContainsExpectedComputation(const InstructionExecutorTestFixture &fixture,
+                                       const ADCAbsolute                    &instruction)
 {
+    EXPECT_THAT(fixture.fakeMemory.at( instruction.address.absolute_address ), Eq( instruction.requirements.initial.addend ));
 }
 
 
-static const std::vector<ADCImmediate> ADCImmediateModeTestValues {
-ADCImmediate{
+static const std::vector<ADCAbsolute> ADCAbsoluteModeTestValues {
+ADCAbsolute{
     // Beginning of a page
-    Immediate().address(0x0000).value(6),
-    ADCImmediate::Requirements{
+    Absolute().address(0x0000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0,
-            .flags = { }},
+            .flags = { },
+            .addend = 6},
         .final = {
             .a = 6,
-            .flags = { }
+            .flags = { },
+            .addend = 6
         }}
 },
-ADCImmediate{
-    // One before the end of a page
-    Immediate().address(0x00FE).value(6),
-    ADCImmediate::Requirements{
+ADCAbsolute{
+    // Middle of a page
+    Absolute().address(0x0088).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0,
-            .flags = { }},
+            .flags = { },
+            .addend = 6},
         .final = {
             .a = 6,
-            .flags = { }
+            .flags = { },
+            .addend = 6
         }}
 },
-ADCImmediate{
-    // Crossing a page boundary
-    Immediate().address(0x00FF).value(6),
-    ADCImmediate::Requirements{
+ADCAbsolute{
+    // End of a page
+    Absolute().address(0x00FD).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0,
-            .flags = { }},
+            .flags = { },
+            .addend = 6},
         .final = {
             .a = 6,
-            .flags = { }
+            .flags = { },
+            .addend = 6
         }}
 },
-ADCImmediate{
-    // Adding a zero does not affect the Z flag
-    Immediate().address(0x8000).value(0x00),
-    ADCImmediate::Requirements{
+ADCAbsolute{
+    // Crossing a page (partial absolute address)
+    Absolute().address(0x00FE).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
-            .a = 6,
-            .flags = { }},
+            .a = 0,
+            .flags = { },
+            .addend = 6},
         .final = {
             .a = 6,
+            .flags = { },
+            .addend = 6
+        }}
+},
+ADCAbsolute{
+    // Crossing a page (entire absolute address)
+    Absolute().address(0x00FF).value(0xA000),
+    ADCAbsolute::Requirements{
+        .initial = {
+            .a = 0,
+            .flags = { },
+            .addend = 6},
+        .final = {
+            .a = 6,
+            .flags = { },
+            .addend = 6
+        }}
+},
+ADCAbsolute{
+    // Adding a zero affects the Z flag
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
+        .initial = {
+            .a = 0x00,
+            .flags = { },
+            .addend = 0},
+        .final = {
+            .a = 0x00,
             .flags = {
                 .n_value = {
                     .status_flag = FLAGS6502::N,
                     .expected_value = false },
                 .z_value = {
                     .status_flag = FLAGS6502::Z,
-                    .expected_value = false },
+                    .expected_value = true },
                 .c_value = {
                     .status_flag = FLAGS6502::C,
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // Adding a negative affects the N flag
-    Immediate().address(0x8000).value(0x80),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
-            .a = 0,
-            .flags = { }},
+            .a = 0x00,
+            .flags = { },
+            .addend = 0x80},
         .final = {
             .a = 0x80,
             .flags = {
@@ -140,17 +183,19 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x80
         }}
 },
 // Carry flag
-ADCImmediate{
+ADCAbsolute{
     // 1 + 1 = 2, C = 0, V=0
-    Immediate().address(0x8000).value(0x01),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x01,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x01},
         .final = {
             .a = 0x02,
             .flags = {
@@ -165,16 +210,18 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x01
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // FF + 1 = 0, C = 1, V = 0
-    Immediate().address(0x8000).value(0x01),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0xFF,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x01},
         .final = {
             .a = 0x00,
             .flags = {
@@ -189,16 +236,18 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x01
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 1 + FF = 0, C = 1, V = 0
-    Immediate().address(0x8000).value(0xFF),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x01,
-            .flags = { }},
+            .flags = { },
+            .addend = 0xFF},
         .final = {
             .a = 0x00,
             .flags = {
@@ -213,16 +262,18 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0xFF
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 7F + 1 = 80, C = 0, V = 1
-    Immediate().address(0x8000).value(0x01),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x7f,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x01},
         .final = {
             .a = 0x80,
             .flags = {
@@ -237,16 +288,18 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = true } }
+                    .expected_value = true } },
+            .addend = 0x01
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0x80 + 0x01 = 0x81 (-128 + 1 = -127), C = 0, V = 0
-    Immediate().address(0x8000).value(0x01),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x80,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x01},
         .final = {
             .a = 0x81,
             .flags = {
@@ -261,16 +314,18 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x01
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0x80 + 0x7F = 0xFF (-128 + 127 = -1), C = 0, V = 0
-    Immediate().address(0x8000).value(0x7F),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x80,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x7F},
         .final = {
             .a = 0xFF,
             .flags = {
@@ -285,16 +340,18 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x7F
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0x80 + 0x80 = 0x00 (-128 + -127 = -256), C = 1, V = 1
-    Immediate().address(0x8000).value(0x80),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x80,
-            .flags = { }},
+            .flags = { },
+            .addend = 0x80},
         .final = {
             .a = 0x00,
             .flags = {
@@ -309,28 +366,18 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = true } }
+                    .expected_value = true } },
+            .addend = 0x80
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0x80 + 0xFF = -129,  V = 1
-    Immediate().address(0x8000).value(0xFF),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x80,
-            .flags = {
-                .n_value = {
-                    .status_flag = FLAGS6502::N,
-                    .expected_value = false },
-                .z_value = {
-                    .status_flag = FLAGS6502::Z,
-                    .expected_value = false },
-                .c_value = {
-                    .status_flag = FLAGS6502::C,
-                    .expected_value = false },
-                .v_value = {
-                    .status_flag = FLAGS6502::V,
-                    .expected_value = false } } },
+            .flags = { },
+            .addend = 0xFF},
         .final = {
             .a = 0x7F,
             .flags = {
@@ -345,13 +392,14 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = true } }
+                    .expected_value = true } },
+            .addend = 0xFF
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0x00 + 0x02 + C = 0x03,
-    Immediate().address(0x8000).value(0x02),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0x02),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0x00,
             .flags = {
@@ -366,7 +414,8 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } } },
+                    .expected_value = false } },
+            .addend = 0x02},
         .final = {
             .a = 0x03,
             .flags = {
@@ -381,13 +430,14 @@ ADCImmediate{
                     .expected_value = false },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0xFF
         }}
 },
-ADCImmediate{
+ADCAbsolute{
     // 0xFF + 0x01 + C = 0x01,
-    Immediate().address(0x8000).value(0x01),
-    ADCImmediate::Requirements{
+    Absolute().address(0x8000).value(0xA000),
+    ADCAbsolute::Requirements{
         .initial = {
             .a = 0xFF,
             .flags = {
@@ -402,7 +452,8 @@ ADCImmediate{
                     .expected_value = true },
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } } },
+                    .expected_value = false } },
+            .addend = 0x01},
         .final = {
             .a = 0x01,
             .flags = {
@@ -417,18 +468,17 @@ ADCImmediate{
                     .expected_value = true }, // Carry should be set because we wrapped around
                 .v_value = {
                     .status_flag = FLAGS6502::V,
-                    .expected_value = false } }
+                    .expected_value = false } },
+            .addend = 0x01
         }}
 }
 };
 
-
-TEST_P(ADCImmediateMode, TypicalInstructionExecution)
+TEST_P(ADCAbsoluteMode, TypicalInstructionExecution)
 {
     TypicalInstructionExecution(*this, GetParam());
 }
 
-INSTANTIATE_TEST_SUITE_P(AddImmediateAtVariousAddresses,
-                         ADCImmediateMode,
-                         testing::ValuesIn(ADCImmediateModeTestValues) );
-
+INSTANTIATE_TEST_SUITE_P(LoadAbsoluteAtVariousAddresses,
+                         ADCAbsoluteMode,
+                         testing::ValuesIn(ADCAbsoluteModeTestValues) );
--- unit_tests/unit_tests.pro
+++ unit_tests/unit_tests.pro
@@ -23,6 +23,7 @@ SOURCES += \
         absolute_indexed_with_x_mode_LDY.cpp \
         absolute_indexed_with_y_mode_LDA.cpp \
         absolute_indexed_with_y_mode_LDX.cpp \
+        absolute_mode_ADC.cpp \
         absolute_mode_LDA.cpp \
         absolute_mode_LDX.cpp \
         absolute_mode_LDY.cpp \
